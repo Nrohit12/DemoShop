da6918d8077a82d62c6e00d1ce2d3c315716ec24
Rohit Raj <rohitraj2731999@gmail.com>
2020-11-21 14:53:06 +0530

    Updates

---
+++ precache-manifest.baa942e4c015eea43af3296231a1ee1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b03ac1d71fca88b1c27b17e7282492ea",
+    "url": "/DemoShop/index.html"
+  },
+  {
+    "revision": "6c3b2b3fd6db273debe7",
+    "url": "/DemoShop/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "809214d3ea10e07fdc26",
+    "url": "/DemoShop/static/css/main.ca374ad9.chunk.css"
+  },
+  {
+    "revision": "6c3b2b3fd6db273debe7",
+    "url": "/DemoShop/static/js/2.3d657f44.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/DemoShop/static/js/2.3d657f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809214d3ea10e07fdc26",
+    "url": "/DemoShop/static/js/main.71a6520b.chunk.js"
+  },
+  {
+    "revision": "240f1a233d58d39fe8d0",
+    "url": "/DemoShop/static/js/runtime-main.24ecde34.js"
+  }
+]);
